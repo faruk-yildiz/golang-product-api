03597880a29c91f17bebee404b3e832e2299432a
Ömer Faruk Yıldız <omerfarukyiildiz@gmail.com>
2024-02-07 14:53:26 +0300

    custom request struct and to model func

---
+++ controller/request/request.go
@@ -0,0 +1,19 @@
+package request
+
+import "product-app/dto"
+
+type AddProductRequest struct {
+	Name     string  `json:"name"`
+	Price    float32 `json:"price"`
+	Discount float32 `json:"discount"`
+	Store    string  `json:"store"`
+}
+
+func (addProductRequest AddProductRequest) ToModel() dto.ProductDto {
+	return dto.ProductDto{
+		Name:     addProductRequest.Name,
+		Price:    addProductRequest.Price,
+		Discount: addProductRequest.Discount,
+		Store:    addProductRequest.Store,
+	}
+}
